a04d84d62ddf172dede49b5e6c7aaa1641ecdf87
matiasfg <matiasfg@effec764-9321-11de-a83d-5bdee9236848>
2009-08-31 17:48:23 +0000

    Changed a little Input's constructor.
    And of course, more cleanup.

--- Input.h
+++ Input.h
@@ -1,52 +1,82 @@
 #pragma once
 
-#pragma warning(disable:4251)
-
-//////////////////////////////////////////////////////////////////////////
-
-#ifdef INPUT_EXPORTS
-#	define INPUT_API __declspec(dllexport)
-#else
-#	define INPUT_API __declspec(dllimport)
-#endif
-
-//////////////////////////////////////////////////////////////////////////
-
-#ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN
-#endif
-
-#ifndef STRICT
-#define STRICT
-#endif
-
-#include <Windows.h>
+#include "RawDevice.h"
+#include "RawInputAPI.h"
 
 #include <exception>
 #include <string>
 #include <vector>
 
-// RawInput index http://msdn.microsoft.com/en-us/library/ms645536%28VS.85%29.aspx
-
 namespace RawInput
 {
 	class INPUT_API Input {
+		Input(const Input &);
+		Input & operator=(const Input &);
 	public:			
 		explicit Input(const HWND hwnd = 0);
 		~Input(void) throw();
 
+		/*
+		 * Basic Update() method.
+		 *
+		 * @param1: LPARAM from main window's WndProc()
+		 */
 		void Update(const LPARAM & lParam);
+
+		/*
+		 * Buffered Update() method, needs work. Pre strict c++
+		 *
+		 * @param1: LPARAM from main window's WndProc()
+		 */
 		//LRESULT UpdateBuff(const LPARAM & lParam); // Bugged?
 
+		/*
+		 * Clean()s the state for all devices. Needs to be called at the end of each loop.
+		 */
+		void Clean(void);
+
+		/*
+		 * KeyUp() returns true on key release.
+		 *
+		 * @param1: one of Keyboard::keyboard_vkeys enum or VK code.
+		 */
 		bool KeyUp(const unsigned short & button) const;
+
+		/*
+		 * KeyDown() returns true on key press.
+		 *
+		 * @param1: one of Keyboard::keyboard_vkeys enum or VK code.
+		 */
 		bool KeyDown(const unsigned short & button) const;
+
+		/*
+		 * KeyHeld() returns true while key is pressed.
+		 *
+		 * @param1: one of Keyboard::keyboard_vkeys enum or VK code.
+		 */
 		bool KeyHeld(const unsigned short & button) const;
 
-		bool MouseEvent(const unsigned short & button) const;
-		void MousePos(long & x, long & y) const;
+		/*
+		 * MouseEvent() returns true on button press.
+		 *
+		 * @param1: one of Mouse::mouse_buttons enum.
+		 */
+		bool MouseEvent(const Mouse::mouse_buttons & button) const;
 
-		void Clean(void);
+		/*
+		 * MousePos() returns mouse's x/y position.
+		 *
+		 * @param1: returns mouse position X.
+		 * @param2: returns mouse position Y.
+		 */
+		void MousePos(long & x, long & y) const;
 	private:
+		/*
+		 * GetSystemDevices() returns a vector with all the devices
+		 * connected to the system at the time of call.
+		 */
+		std::vector<RAWINPUTDEVICELIST> GetSystemDevices(void) const;
+
 		//http://www.microsoft.com/whdc/archive/HID_HWID.mspx
 		const static unsigned short HID_USAGE_PAGE				= 0x01;
 
@@ -57,75 +87,10 @@ namespace RawInput
 
 		std::vector<RAWINPUTDEVICE> ri_devices_;
 
-		RAWMOUSE		ri_mouse_;
-		RAWKEYBOARD		ri_keyboard_;
-		RAWHID			ri_hid_;
+		RAWMOUSE	ri_mouse_;
+		RAWKEYBOARD	ri_keyboard_;
+		RAWHID		ri_hid_;
 	};
-
-	namespace Mouse
-	{
-		const enum mouse_buttons : unsigned short {
-			BUTTON0_DOWN	= RI_MOUSE_BUTTON_1_DOWN,
-			BUTTON0_UP		= RI_MOUSE_BUTTON_1_UP,
-
-			BUTTON1_DOWN	= RI_MOUSE_BUTTON_2_DOWN,
-			BUTTON1_UP		= RI_MOUSE_BUTTON_2_UP,
-
-			BUTTON2_DOWN	= RI_MOUSE_BUTTON_3_DOWN,
-			BUTTON2_UP		= RI_MOUSE_BUTTON_3_UP,
-
-			BUTTON3_DOWN	= RI_MOUSE_BUTTON_4_DOWN,
-			BUTTON3_UP		= RI_MOUSE_BUTTON_4_UP,
-
-			BUTTON4_DOWN	= RI_MOUSE_BUTTON_5_DOWN,
-			BUTTON4_UP		= RI_MOUSE_BUTTON_5_UP,
-
-			WHEEL			= RI_MOUSE_WHEEL
-		};
-	}
-
-	// VKey codes http://msdn.microsoft.com/en-us/library/ms645540%28VS.85%29.aspx
-	namespace Keyboard
-	{
-		const enum keyboard_vkeys : unsigned short {
-			VK_0 = 0x30,
-			VK_1,
-			VK_2,
-			VK_3,
-			VK_4,
-			VK_5,
-			VK_6,
-			VK_7,
-			VK_8,
-			VK_9,
-			VK_A = 0x41,
-			VK_B,
-			VK_C,
-			VK_D,
-			VK_E,
-			VK_F,
-			VK_G,
-			VK_H,
-			VK_I,
-			VK_J,
-			VK_K,
-			VK_L,
-			VK_M,
-			VK_N,
-			VK_O,
-			VK_P,
-			VK_Q,
-			VK_R,
-			VK_S,
-			VK_T,
-			VK_U,
-			VK_V,
-			VK_W,
-			VK_X,
-			VK_Y,
-			VK_Z
-		};
-	}
 
 	struct Input_Exception : public std::exception {
 		const std::wstring error_msg;
