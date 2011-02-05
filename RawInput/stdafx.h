1bf060bf7f78585d20813168854e9589a326a689
MatiasFG <MatiasFG@effec764-9321-11de-a83d-5bdee9236848>
2011-02-05 04:35:20 +0000

    - Raised warnings to level 4.
    - More code cleanup.

--- RawInput/stdafx.h
+++ RawInput/stdafx.h
@@ -1,6 +1,7 @@
 #pragma once
 
-//////////////////////////////////////////////////////////////////////////
+#ifndef stdafx_h__
+#define stdafx_h__
 
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
@@ -10,6 +11,8 @@
 #define STRICT
 #endif
 
+#pragma warning(push, 0)
 #include <Windows.h>
+#pragma warning(pop)
 
-//////////////////////////////////////////////////////////////////////////
+#endif // stdafx_h__
