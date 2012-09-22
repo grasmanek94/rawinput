d6bde4c5ac6c71791e00dd8a80c60d95fb688874
matiasfg <matiasfg@effec764-9321-11de-a83d-5bdee9236848>
2012-09-22 19:09:53 +0000

    + Fundamental changes to the way input is queried. Now, any new input will trigger events that users can subscribe to. - Fixed error when new device was added and improved new input handling a bit. - Building with Visual Studio 2012, using some C++11 features.

--- RawInput/RawKeyboard.h
+++ RawInput/RawKeyboard.h
@@ -1,7 +1,9 @@
 #ifndef RawKeyboard_h__
 #define RawKeyboard_h__
 
-#include "rawdevice.h"
+#include "RawInputAPI.h"
+
+#include "RawDevice.h"
 
 namespace RawInput
 {
@@ -17,26 +19,51 @@ namespace RawInput
 
 		const RAWKEYBOARD & GetData(void) const;
 
-		/*
+		/**
 		 * KeyUp() returns true on key release.
 		 *
 		 * @param1: one VK code.
 		 */
 		bool KeyUp(unsigned short) const;
 
-		/*
+		/**
 		 * KeyDown() returns true on key press.
 		 *
 		 * @param1: one VK code.
 		 */
 		bool KeyDown(unsigned short) const;
 
-		/*
+		/**
 		 * KeyHeld() returns true while key is pressed.
 		 *
 		 * @param1: one VK code.
 		 */
 		bool KeyHeld(unsigned short) const;
+
+	public:
+		typedef BaseEvent<RawKeyboard> Event;
+
+		template <class Callable>
+		class Connect : public Event
+		{
+		public:
+			Connect(Callable & callable)
+				: m_callable(callable) {
+			}
+
+			virtual void operator()(const Event::DeviceType & dev) override {
+				m_callable(dev);
+			}
+
+		private:
+			Connect(const Connect &);
+
+			Connect & operator=(const Connect &);
+
+		private:
+			const Callable & m_callable;
+		};
+
 	private:
 		RAWKEYBOARD m_data;
 	};
